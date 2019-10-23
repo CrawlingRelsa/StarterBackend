bc1abceb839cb7e06d8149f0edc30b851af2760a
amine <a.elasraoui@reply.it>
2019-10-23 13:00:03 +0200

    env config

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import morgan from 'morgan'
-import testRoute from './route/sample'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import path from "path";
+
+//routes
+import testRoute from "./route/sample";
 
 function main() {
-    let app = express()
-    //setup express
-    app.use(morgan('[:date[web]] :method :url :status'))
-    app.use(bodyParser.json()) // for parsing application/json
-    app.use(cors()) //to avoid CORS problems
-    app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-    //---routes
-    app.use('/test', testRoute)
-
-    app.listen(3000, () => {
-        console.log('server on')
-    })
+  dotenv.config({ path: path.join(process.cwd(), "env", ".env") }); //you can choose the env file here
+  let app = express();
+  //setup express
+  app.use(morgan("[:date[web]] :method :url :status"));
+  app.use(bodyParser.json()); // for parsing application/json
+  app.use(cors()); //to avoid CORS problems
+  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+  //---routes
+  app.use("/test", testRoute);
+  app.listen(process.env.PORT, () => {
+    console.log("server ready at", process.env.PORT);
+  });
 }
-main()
-
+main();
